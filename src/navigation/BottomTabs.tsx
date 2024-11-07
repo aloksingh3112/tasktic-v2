eac8c1043d550e3925ce16e1f64360aa5bb51c62
abhisheksingh98 <writetoabhishek07@gmail.com>
2024-11-08 02:48:28 +0530

    chore: boilerplate cleanup

--- src/navigation/BottomTabs.tsx
+++ src/navigation/BottomTabs.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from '../screens/HomeScreen';
-import CalendarScreen from '../screens/CalendarScreen';
-import GoalsScreen from '../screens/GoalsScreen';
-import CommunityScreen from '../screens/CommunityScreen';
-import ProgressScreen from '../screens/ProgressScreen';
-import { Ionicons } from '@expo/vector-icons';
-import colors from '../theme/color';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens/HomeScreen";
+import CalendarScreen from "../screens/CalendarScreen";
+import GoalsScreen from "../screens/GoalsScreen";
+import CommunityScreen from "../screens/CommunityScreen";
+import ProgressScreen from "../screens/ProgressScreen";
+import { Ionicons } from "@expo/vector-icons";
+import colors from "../theme/color";
 
 const Tab = createBottomTabNavigator();
 
@@ -17,24 +17,24 @@ const BottomTabs = () => (
         let iconName;
 
         switch (route.name) {
-          case 'Home':
-            iconName = 'home-outline';
+          case "Home":
+            iconName = "home-outline";
             break;
-          case 'Calendar':
-            iconName = 'calendar-outline';
+          case "Calendar":
+            iconName = "calendar-outline";
             break;
-          case 'Goals':
-            iconName = 'trophy-outline';
+          case "Goals":
+            iconName = "trophy-outline";
             break;
-          case 'Community':
-            iconName = 'people-outline';
+          case "Community":
+            iconName = "people-outline";
             break;
-          case 'Progress':
-            iconName = 'bar-chart-outline';
+          case "Progress":
+            iconName = "bar-chart-outline";
             break;
         }
 
-        return <Ionicons name={iconName} size={size} color={color} />;
+        return <Ionicons size={size} color={color} />;
       },
       tabBarActiveTintColor: colors.primary,
       tabBarInactiveTintColor: colors.textSecondary,
